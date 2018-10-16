ff8bced410d122eef7ccd34218153f83dc1b132a
Oliver Richman <oliver.richman@outlook.com>
2018-10-16 15:51:19 +0100

    login

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-sign-in',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-	constructor() { }
+	constructor(private router: Router) { }
+
+	username = '';
+	password = '';
 
 	ngOnInit() {
+		if (localStorage.getItem('sessionId') === '999') {
+			this.router.navigate(['']);
+		}
+	}
+
+	onSubmit(value: any) {
+		this.username = value.username;
+		this.password = value.password;
+		if (this.username === 'admin' && this.password === 'password') {
+			const sessionId: string = Math.random().toString();
+			localStorage.setItem('sessionId', '999');
+			this.router.navigate(['']);
+		}
 	}
 
 }
