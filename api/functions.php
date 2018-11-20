6d3c18ffed9c6e70e26c963c54f9f91e4da8b95b
Oliver Richman <oliver.richman@outlook.com>
2018-11-20 14:59:01 +0000

    updates to the image uploading system

--- api/functions.php
+++ api/functions.php
@@ -12,9 +12,12 @@
 
     function uploadProfileImage($files, $post)
     {
+        $fileExtension = explode($files["fileToUpload"]["name"])[1];
+        $files["fileToUpload"]["name"] = uniqid('');
         $db = $GLOBALS['db'];
         $target_dir = "images/";
-        $target_file = $target_dir . basename($files["fileToUpload"]["name"]);
+        $target_file = $target_dir . uniqid('') . "." . $fileExtension;
+
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
         // Check if image file is a actual image or fake image
@@ -57,7 +60,7 @@
         if ($uploadOk == 1) {
             if (move_uploaded_file($files["fileToUpload"]["tmp_name"], $target_file)) {
                 echo "The file ". basename($files["fileToUpload"]["name"]). " has been uploaded.";
-                $profileURL = "http://highland.oliverrichman.uk/api/images/".$files["fileToUpload"]["name"];
+                $profileURL = $files["fileToUpload"]["name"].".".$fileExtension;
                 $postImgUrlQuery = "UPDATE `users` SET `profile_img` = '".$profileURL."' WHERE `id` = ".$post['id'];
                 $postImgUrlResult = mysqli_query($db, $postImgUrlQuery);
             } else {
@@ -77,24 +80,6 @@
         $uploadOk = 1;
         $videoFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-
-        // // Check if image file is a actual image or fake image
-        // if (isset($post["submit"])) {
-        //     $check = getimagesize($files["fileToUpload"]["tmp_name"]);
-        //     // print_r($check);
-        //     if ($check !== false) {
-        //         $uploadOk = 1;
-        //     } else {
-        //         header("HTTP/1.0 400 Bad Request");
-        //         response(400, "Not a Video", true);
-        //         $uploadOk = 0;
-        //     }
-        // }
-        // if ($check[0] > 500 || $check[1] > 500) {
-        //     header("HTTP/1.0 400 Bad Request");
-        //     response(400, "File too large, must be 500 x 500", true);
-        //     $uploadOk = 0;
-        // }
         // Check if file already exists
         if (file_exists($target_file)) {
             header("HTTP/1.0 400 Bad Request");
