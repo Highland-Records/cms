6e4bf47032260faf3286ae3e74f70b7500ccef41
Tom Smedley <tsmedley@uclan.ac.uk>
2018-11-29 16:17:56 +0000

    Front-end rewritten and re styled

--- app/src/comps/Splash/Home.js
+++ app/src/comps/Splash/Home.js
@@ -4,12 +4,66 @@ import HomeNavigation from "./Navigation";
 
 // - Home Splash Page
 class SplashHome extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
 		return (
 			<div className="App">
 				<section className="SplashStyle">
 					{HomeNavigation.DrawNavigation()}
-					<div class="banner"></div>
+					<div class="banner home">
+						feel <b>something</b>
+					</div>
+					<ul className="home">
+						<li>
+							Today's featured Artist
+						</li>
+						<li>
+							<div>
+								<h1>
+									Artist name
+								</h1>
+								<ul>
+									<h1>Recent releases</h1>
+									<li>SONG TITLE</li>
+									<li>SONG TITLE</li>
+									<li>SONG TITLE</li>
+									<li>SONG TITLE</li>
+									<li>SONG TITLE</li>
+								</ul>
+							</div>
+						</li>
+					</ul>
+					<div className="list">
+						<h1>
+							Our Artists
+						</h1>
+						<ul className="list">
+							<li>
+								<img src="https://via.placeholder.com/200" />
+								<h2>Artist Name</h2>
+							</li>
+							<li>
+								<img src="https://via.placeholder.com/200" />
+								<h2>Artist Name</h2>
+							</li>
+							<li>
+								<img src="https://via.placeholder.com/200" />
+								<h2>Artist Name</h2>
+							</li>
+							<li>
+								<img src="https://via.placeholder.com/200" />
+								<h2>Artist Name</h2>
+							</li>
+						</ul>
+					</div>
+					<footer>
+						<div className="c">
+							<i>Highland</i>
+							<p>&copy; 2018 Highland Records</p>
+						</div>
+					</footer>
 				</section>
 			</div>
 		);
